98ee877fd36e38990d481fd938dca6376cce85d9
Sakamoto Osamu <saka1029@gmail.com>
2015-05-27 06:41:58 +0900

    Eliminate duplicated lists

--- src/combination/Combination.java
+++ src/combination/Combination.java
@@ -2,25 +2,49 @@ package combination;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class Combination {
 
-	public static <T> void found(int[] combination, List<T> elements, List<T> result, Consumer<List<T>> callback) {
-		int length = combination.length;
-		for (int i = 0; i < length; ++i)
-			result.set(i, elements.get(combination[i]));
+	private static <T> Set<T> duplicated(List<T> elements) {
+		Map<T, Integer> count = new HashMap<>();
+		for (T e : elements) {
+			Integer i = count.get(e);
+			if (i == null) i = 0;
+			count.put(e, i + 1);
+		}
+		Set<T> result = new HashSet<>();
+		for (Entry<T, Integer> e : count.entrySet())
+			if (e.getValue() > 1)
+				result.add(e.getKey());
+		return result;
+	}
+
+	private static <T> void found(int[] combination, List<T> elements,
+			Set<T> duplicated, Set<List<T>> pool, Consumer<List<T>> callback) {
+		List<T> result = new ArrayList<>();
+		for (int i : combination)
+			result.add(elements.get(i));
+		for (T e : result)
+			if (duplicated.contains(e)) {
+				pool.add(result);
+				return;
+			}
 		callback.accept(result);
 	}
 
-	public static <T> void combination(List<T> elements, int k, Consumer<List<T>> callback) {
-		List<T> result = new ArrayList<>(k);
-		for (int i = 0; i < k; ++i)
-			result.add(null);
+	private static <T> void combination(List<T> elements, int k,
+			Consumer<List<T>> callback, Set<T> duplicated) {
+		Set<List<T>> pool = new HashSet<>();
 		int n = elements.size();
 		if(k > n)
-			throw new IllegalArgumentException("Invalid input, k > n");
+			throw new IllegalArgumentException("k must <= elements size");
 		int combination[] = new int[k];
 		int r = 0;		
 		int index = 0;
@@ -29,40 +53,30 @@ public class Combination {
                 combination[r] = index;
 				if(r == k - 1) {
 					index++;
-					found(combination, elements, result, callback);
+					found(combination, elements, duplicated, pool, callback);
 				} else
 					index = combination[r++] + 1;
 			} else if(--r >= 0)
                 index = combination[r] + 1;
 		}
+//		System.out.println("pool=" + pool);
+		for (List<T> e : pool)
+			callback.accept(e);
 	}
 
-	static <T> void fold(List<T> list, List<T> folded, List<Integer> count) {
-		folded.clear();
-		count.clear();
-		for (T e : list) {
-			int i = folded.indexOf(e);
-			if (i == -1) {
-				folded.add(e);
-				count.add(1);
-			} else
-				count.set(i, count.get(i) + 1);
-		}
-		
+	public static <T> void allCombinations(List<T> elements, Consumer<List<T>> callback) {
+		Set<T> duplicated = duplicated(elements);
+//		System.out.println("duplicated=" + duplicated);
+		for (int i = 1, size = elements.size(); i <= size; ++i)
+            combination(elements, i, callback, duplicated);
 	}
 
-	public static void testCombination() {
-		List<Integer> elements = Arrays.asList(1, 1, 2, 3);
-		List<Integer> folded = new ArrayList<>();
-		List<Integer> count = new ArrayList<>();
-		fold(elements, folded, count);
-		System.out.printf("elements = %s folded = %s count=%s%n", elements, folded, count);
-		for (int i = 1, max  = elements.size(); i <= max; ++i)
-            combination(elements, i, x -> { System.out.println(x); });
-	}
-		
-	public static void main(String[] args){
-		testCombination();
+	public static void main(String[] args) {
+		allCombinations(Arrays.asList(1, 1, 2, 3),
+			list -> {
+                System.out.println(list);
+            }
+		);
 	}
 	
 }
