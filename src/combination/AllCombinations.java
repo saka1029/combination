7f12bac784f740c5aec9f6c4ecf233f63bdac660
saka1029 <saka1029@gmail.com>
2016-01-09 07:37:51 +0900

    aa

--- src/combination/AllCombinations.java
+++ src/combination/AllCombinations.java
@@ -16,124 +16,124 @@ import java.util.stream.StreamSupport;
 
 public class AllCombinations<T> {
 
-	final List<T> uniqs = new ArrayList<>();
-	final List<Integer> sizes = new ArrayList<>();
-	final int max;
-	final int[] counts;
-	final Consumer<List<T>> callback;
+    final List<T> uniqs = new ArrayList<>();
+    final List<Integer> sizes = new ArrayList<>();
+    final int max;
+    final int[] counts;
+    final Consumer<List<T>> callback;
 
-	private AllCombinations(List<T> elements, Consumer<List<T>> callback) {
-		Map<T, Integer> map = new HashMap<>();
-		for (T e : elements) {
+    private AllCombinations(List<T> elements, Consumer<List<T>> callback) {
+        Map<T, Integer> map = new HashMap<>();
+        for (T e : elements) {
             Integer i = map.get(e);
             if (i == null) i = 0;
             map.put(e, i + 1);
-		}
-		for (Entry<T, Integer> e : map.entrySet()) {
-			this.uniqs.add(e.getKey());
-			this.sizes.add(e.getValue());
-		}
-		this.max = uniqs.size();
-		this.counts = new int[max];
-		this.callback = callback;
-	}
+        }
+        for (Entry<T, Integer> e : map.entrySet()) {
+            this.uniqs.add(e.getKey());
+            this.sizes.add(e.getValue());
+        }
+        this.max = uniqs.size();
+        this.counts = new int[max];
+        this.callback = callback;
+    }
 
-	private AllCombinations(List<T> elements) {
-		this(elements, null);
-	}
+    private AllCombinations(List<T> elements) {
+        this(elements, null);
+    }
 
-	private List<T> result() {
-		List<T> list = new ArrayList<>();
-		for (int i = 0; i < max; ++i) {
-			T value = uniqs.get(i);
-			for (int j = 0, size = counts[i]; j < size; ++j)
-				list.add(value);
-		}
-		return list;
-	}
-	private void found(int total) {
-//		System.out.println("answer=" + Arrays.toString(counts));
-		if (total <= 0) return;
-		List<T> list = result();
-		callback.accept(list);
-	}
+    private List<T> result() {
+        List<T> list = new ArrayList<>();
+        for (int i = 0; i < max; ++i) {
+            T value = uniqs.get(i);
+            for (int j = 0, size = counts[i]; j < size; ++j)
+                list.add(value);
+        }
+        return list;
+    }
+    private void found(int total) {
+//        System.out.println("answer=" + Arrays.toString(counts));
+        if (total <= 0) return;
+        List<T> list = result();
+        callback.accept(list);
+    }
 
-	private void solve(int i, int total) {
-		if (i >= max)
+    private void solve(int i, int total) {
+        if (i >= max)
             found(total);
-		else
-			for (int k = 0, size = sizes.get(i); k <= size; ++k) {
-				counts[i] = k;
-				solve(i + 1, total + k);
-			}
-	}
+        else
+            for (int k = 0, size = sizes.get(i); k <= size; ++k) {
+                counts[i] = k;
+                solve(i + 1, total + k);
+            }
+    }
 
-	public static <T> void allCombinations(List<T> elements, Consumer<List<T>> callback) {
-		new AllCombinations<T>(elements, callback).solve(0, 0);
-	}
-	
-	public static <T> Iterator<List<T>> iterator(List<T> elements) {
-		return new Iterator<List<T>>() {
-			
-			AllCombinations<T> a = new AllCombinations<>(elements);
+    public static <T> void allCombinations(List<T> elements, Consumer<List<T>> callback) {
+        new AllCombinations<T>(elements, callback).solve(0, 0);
+    }
+    
+    public static <T> Iterator<List<T>> iterator(List<T> elements) {
+        return new Iterator<List<T>>() {
+            
+            AllCombinations<T> a = new AllCombinations<>(elements);
 
-			@Override
-			public boolean hasNext() {
-				for (int i = 0; i < a.max; ++i)
-					if (a.counts[i] < a.sizes.get(i))
-						return true;
-				return false;
-			}
+            @Override
+            public boolean hasNext() {
+                for (int i = 0; i < a.max; ++i)
+                    if (a.counts[i] < a.sizes.get(i))
+                        return true;
+                return false;
+            }
 
-			List<T> found(int index) {
-				++a.counts[index];
-				return a.result();
-			}
+            List<T> found(int index) {
+                ++a.counts[index];
+                return a.result();
+            }
 
-			@Override
-			public List<T> next() {
-				for (int i = 0; i < a.max; ++i)
-					if (a.counts[i] < a.sizes.get(i))
-						return found(i);
-					else
-						a.counts[i] = 0;
-				throw new NoSuchElementException();
-			}
-		};
-	}
+            @Override
+            public List<T> next() {
+                for (int i = 0; i < a.max; ++i)
+                    if (a.counts[i] < a.sizes.get(i))
+                        return found(i);
+                    else
+                        a.counts[i] = 0;
+                throw new NoSuchElementException();
+            }
+        };
+    }
 
-	public static <T> Iterable<List<T>> iterable(List<T> elements) {
-		return new Iterable<List<T>>() {
-			@Override
-			public Iterator<List<T>> iterator() {
-				return AllCombinations.iterator(elements);
-			}
-		};
-	}
+    public static <T> Iterable<List<T>> iterable(List<T> elements) {
+        return new Iterable<List<T>>() {
+            @Override
+            public Iterator<List<T>> iterator() {
+                return AllCombinations.iterator(elements);
+            }
+        };
+    }
 
-	public static <T> Stream<List<T>> stream(List<T> elements) {
-		return StreamSupport.stream(
+    public static <T> Stream<List<T>> stream(List<T> elements) {
+        return StreamSupport.stream(
             Spliterators.spliteratorUnknownSize(iterator(elements), Spliterator.ORDERED),
             false);
-	}
-	
-	public static void main(String[] args) {
-		List<Integer> elements = Arrays.asList(1, 1, 2, 3);
-		System.out.println("---- callback ----");
-		AllCombinations.allCombinations(elements, list -> {
-			System.out.println(list);
-		});
-		System.out.println("---- iterator ----");
-		Iterator<List<Integer>> it = AllCombinations.iterator(elements);
-		while (it.hasNext())
-			System.out.println(it.next());
+    }
+    
+    public static void main(String[] args) {
+        List<Integer> elements = Arrays.asList(1, 1, 2, 3);
+        System.out.println("---- callback ----");
+        AllCombinations.allCombinations(elements, list -> {
+            System.out.println(list);
+        });
+        System.out.println("---- iterator ----");
+        Iterator<List<Integer>> it = AllCombinations.iterator(elements);
+        while (it.hasNext())
+            System.out.println(it.next());
 
-		System.out.println("---- iterable ----");
-		for (List<Integer> list : AllCombinations.iterable(elements))
-			System.out.println(list);
+        System.out.println("---- iterable ----");
+        for (List<Integer> list : AllCombinations.iterable(elements))
+            System.out.println(list);
 
-		System.out.println("---- stream ----");
-		AllCombinations.stream(elements)
-			.forEach(System.out::println);
-	}
+        System.out.println("---- stream ----");
+        AllCombinations.stream(elements)
+            .forEach(System.out::println);
+    }
 }
